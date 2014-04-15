c64c8cf26190201bc29607ff5da2ce3f31c9429b
lulalala <mark@goodlife.tw>
2014-04-15 17:45:46 +0800

    Follow standard interface and consume raw_post instead of params from Rails. 父類別與其他實作都是接受 raw_post，所以遵循此介面。 父類別也有定義 params 變數跟其 parse 實作，可直接使用。

--- lib/active_merchant/billing/integrations/allpay/notification.rb
+++ lib/active_merchant/billing/integrations/allpay/notification.rb
@@ -32,16 +32,6 @@ module ActiveMerchant #:nodoc:
             (Digest::MD5.hexdigest(url_endcode_data) == checksum.to_s.downcase)
           end
 
-          def initialize(params)
-            self.params = params
-          end
-
-          def params=(params)
-            @params = params.inject({}) do |buffer, (name, value)|
-              buffer.merge(name.to_s => value)
-            end
-          end
-
           def rtn_code
             @params['RtnCode']
           end
@@ -88,17 +78,6 @@ module ActiveMerchant #:nodoc:
 
           def check_mac_value
             @params['CheckMacValue']
-          end
-
-          private
-
-          # Take the posted data and move the relevant data into a hash
-          def parse(post)
-            @raw = post.to_s
-            for line in @raw.split('&')
-              key, value  = *line.scan(%r{^([A-Za-z0-9_.]+)\=(.*)$}).flatten
-              params[key] = CGI.unescape(value)
-            end
           end
 
         end
